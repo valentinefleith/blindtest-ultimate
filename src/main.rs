c7277ae5c9a954c42a547f869b3a91ac9b9ba249
valentinefleith <valentine.fleith@gmail.com>
2025-01-27 01:52:22 +0100

    hello world rust serv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+async fn hello_blindtest() -> &'static str {
+    "Hello Blindtest!"
+}
+
+#[tokio::main]
+async fn main() {
+    let router = Router::new().route("/", get(hello_blindtest));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let tcp = TcpListener::bind(&addr).await.unwrap();
+    println!("Serveur en cours d'exécution sur http://{}", addr);
+    axum::serve(tcp, router).await.unwrap();
 }
