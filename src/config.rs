a9d8d423b8d575a7f322e8583f38e1609c0e082b
valentinefleith <valentine.fleith@gmail.com>
2025-01-28 16:28:15 +0100

    remove rust i'm giving up

--- src/config.rs
+++
@@ -1,7 +0,0 @@
-use sqlx::SqlitePool;
-use std::sync::Arc;
-
-pub async fn init_db(database_url: &str) -> Arc<SqlitePool> {
-    let pool = SqlitePool::connect(database_url).await.unwrap();
-    Arc::new(pool)
-}
--- src/routes/mod.rs
+++
@@ -1,7 +0,0 @@
-use axum::Router;
-
-mod users;
-
-pub fn create_router() -> Router {
-    Router::new().nest("/api/users", users::routes())
-}
