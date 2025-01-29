fe37fd1922ee8fa340b4bd157a96a27737ec21a0
valentinefleith <valentine.fleith@gmail.com>
2025-01-29 02:29:51 +0100

    add registration and connection from the website

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes), // Gère le système de navigation Angular
+    provideRouter(routes),
+    importProvidersFrom(HttpClientModule, FormsModule), // 👈 Ajout de FormsModule
   ],
 }).catch(err => console.error(err));
