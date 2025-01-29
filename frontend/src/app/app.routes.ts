ddf86f3391a77d328b46ca7feb2a8a16b8799dc5
valentinefleith <valentine.fleith@gmail.com>
2025-01-29 02:04:43 +0100

    add pages for authentification

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }, // Page d'accueil
+  { path: '', component: HomeComponent }, // Accueil
+  { path: 'register', component: RegisterComponent }, // Inscription
+  { path: 'login', component: LoginComponent }, // Connexion
 ];
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router'; // Import du module de routing
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true, // Important pour Angular 17+
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'],
+  imports: [RouterModule], // Ajout explicite du module de routing
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
