c28d92e98cdc1f466119cae4d0473500b9aa13e5
valentinefleith <valentine.fleith@gmail.com>
2025-01-31 03:56:58 +0100

    Refactor and enhance home and navbar components.
    Added authentication state handling to the home component and updated styles for better responsiveness and alignment. Changed app text and placeholders for improved localization. Standardized global theming and adjusted structural elements for a more consistent user experience.

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router'; // Import du module de routing
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {CommonModule} from '@angular/common'; // Import du module de routing
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
   standalone: true, // Important pour Angular 17+
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [RouterModule], // Ajout explicite du module de routing
+  imports: [RouterModule, CommonModule], // Ajout explicite du module de routing
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  isLoggedIn = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.isLoggedIn$.subscribe(status => {
+      this.isLoggedIn = status
+    });
+  }
+}
